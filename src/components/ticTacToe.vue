cd3912debac83ead245131e21870e87ee27afead
dhfernando <hdilshan.fernando11@gmail.com>
2020-03-15 22:09:20 +0530

    make x & o chage

--- src/components/ticTacToe.vue
+++ src/components/ticTacToe.vue
@@ -1,40 +1,39 @@
 <template>
   <div class="row container" >
     <div class="col-6 border ml-5">
-
       <div class="row">
         <div class="col">
-            <button @click="clicked()">1</button>
+            <button @click="clicked(1)" value = 1 id = 1></button>
         </div>
         <div class="col">
-            <button @click="clicked()">1</button>
+            <button @click="clicked(2)" id = 2>1</button>
         </div>
         <div class="col">
-            <button @click="clicked()">1</button>
+            <button @click="clicked(3)" id = 3>1</button>
         </div>
       </div>
 
       <div class="row">
         <div class="col">
-            <button @click="clicked()">1</button>
+            <button @click="clicked(4)" id = 4>1</button>
         </div>
         <div class="col">
-            <button @click="clicked()">1</button>
+            <button @click="clicked(5)" id = 5>1</button>
         </div>
         <div class="col">
-            <button @click="clicked()">1</button>
+            <button @click="clicked(6)" id = 6>1</button>
         </div>
       </div>
 
       <div class="row">
         <div class="col">
-            <button @click="clicked()">1</button>
+            <button @click="clicked(7)" id = 7>1</button>
         </div>
         <div class="col">
-            <button @click="clicked()">1</button>
+            <button @click="clicked(8)" id = 8>1</button>
         </div>
         <div class="col">
-            <button @click="clicked()">1</button>
+            <button @click="clicked(9)" id = 9>1</button>
         </div>
       </div>
 
@@ -49,15 +48,30 @@ export default {
   
   data(){
       return{
-        
+        count : 0,
+        player:{
+          One : 'Dilshan',
+          Two : 'Gayathri'
+        }
       }
     },
     computed: {
        
     },
     methods:{
-      clicked(){
-        alert('clicked')
+      matricks(){
+        []
+      },
+      clicked(e){
+        this.count ++;
+
+        document.getElementById(e).disabled = true
+        if(this.count % 2 ){
+          document.getElementById(e).innerHTML = "X" 
+        } else{
+          document.getElementById(e).innerHTML = "O" 
+        }
+        
       }
     }
 }
