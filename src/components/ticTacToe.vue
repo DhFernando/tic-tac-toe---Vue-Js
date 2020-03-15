cfbef4f4c0b174057593275fa4e93fbc3b245a09
dhfernando <hdilshan.fernando11@gmail.com>
2020-03-15 21:25:38 +0530

    init tic tac toe

---
+++ src/components/ticTacToe.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="row container" >
+    <div class="col-6 border ml-5">
+
+      <div class="row">
+        <div class="col">
+            <button @click="clicked()">1</button>
+        </div>
+        <div class="col">
+            <button @click="clicked()">1</button>
+        </div>
+        <div class="col">
+            <button @click="clicked()">1</button>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+            <button @click="clicked()">1</button>
+        </div>
+        <div class="col">
+            <button @click="clicked()">1</button>
+        </div>
+        <div class="col">
+            <button @click="clicked()">1</button>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+            <button @click="clicked()">1</button>
+        </div>
+        <div class="col">
+            <button @click="clicked()">1</button>
+        </div>
+        <div class="col">
+            <button @click="clicked()">1</button>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TicTacToe',
+  
+  data(){
+      return{
+        
+      }
+    },
+    computed: {
+       
+    },
+    methods:{
+      clicked(){
+        alert('clicked')
+      }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
